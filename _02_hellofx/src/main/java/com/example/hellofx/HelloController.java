c3b402bdffe5b7a68d2b31653d5541b25e7f5c81
Micha Schirmer <michajava8@gmail.com>
2025-11-25 14:16:55 +0100

    JavaFX

---
+++ _02_hellofx/src/main/java/com/example/hellofx/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.hellofx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private Label label;//fx-id
+
+    @FXML
+    private TextField textfield;//fx-id
+
+    @FXML
+    void onClick(ActionEvent event) {
+        label.setText(textfield.getText());
+    }
+
+    @FXML
+    void initialize(){
+        //wird nach den Konstruktor aufgerufen
+        System.out.println("init...");
+    }
+
+}
