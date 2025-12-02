45ec75e996e7e4d49a785062e63af66f2fb61f6b
Micha Schirmer <michajava8@gmail.com>
2025-12-02 10:47:14 +0100

    TodoFX Datenbank SQlite TodoApplication close DB-Connection

--- TodoFX_v03/src/main/java/com/example/todofx/TodoApplication.java
+++ TodoFX_v03/src/main/java/com/example/todofx/TodoApplication.java
@@ -1,11 +1,13 @@
 package com.example.todofx;
 
+import com.example.todofx.db.DBConnect;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class TodoApplication extends Application {
     @Override
@@ -16,5 +18,13 @@ public class TodoApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(event -> {
+            System.out.println("close");
+            try {
+                DBConnect.getInstance().connect().close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
